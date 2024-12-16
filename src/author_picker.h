7587c6611134b19f482840ec877eacce55313e1f
Gab-San <santandreagabriele@gmail.com>
2024-12-16 09:59:53 +0100

    Implementing remove command
    It is now possible to remove an author directly from terminal.
    The remotion locates only completely matching strings: the limitation are those of the insertion.
    Like insertion, right now the remove action fails and closes if a buffer of length 0 is passed.
    
    README must be updated.
    Docs should be updated.

--- src/author_picker.h
+++ src/author_picker.h
@@ -20,10 +20,10 @@ void insert_author(char* author_name);
 */
 void extract_author();
 
-// /*
-//     Searches and removes from the file the selected author's name.
-// */
-// void remove_author(char* author_name);
+/*
+    Searches and removes from the file the selected author's name.
+*/
+void remove_author(char* author_name);
 
 /*
     Prints out all of the authors currently inserted
